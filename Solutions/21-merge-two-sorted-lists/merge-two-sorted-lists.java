96dfdc09673e621f8b79829229cc32070a0fd97d
Daniel Marques <dmmarques2005@gmail.com>
2025-06-12 21:06:41 -0300

    Add files via upload

---
+++ Solutions/21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Create a dummy node to start the merged list
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        // While neither list is empty, compare and append the smaller node
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // Append the remaining elements (only one list will have nodes left)
+        current.next = (list1 != null) ? list1 : list2;
+
+        // Return the next of dummy node as the actual head
+        return dummy.next;
+    }
+}
