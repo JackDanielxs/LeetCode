21054817f49b8b71f7b00bee218e7c932402edce
Daniel Marques <dmmarques2005@gmail.com>
2025-06-12 21:36:54 -0300

    Delete 55-jump-game directory

--- 55-jump-game/jump-game.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def canJump(self, nums: list[int]) -> bool:
-        last_pos = len(nums) - 1
-        for i in range(len(nums) - 2, -1, -1):
-            if i + nums[i] >= last_pos:
-                last_pos = i
-        return last_pos == 0
