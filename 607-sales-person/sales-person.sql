19af540ba9ca5491ca58d548da5c7fb95b1e4df3
Daniel Marques <dmmarques2005@gmail.com>
2025-06-12 21:38:26 -0300

    Delete 607-sales-person directory

--- 607-sales-person/sales-person.sql
+++
@@ -1,9 +0,0 @@
-/* Write your T-SQL query statement below */
-select s.name 
-from SalesPerson s
-where s.sales_id not in 
-(
-    select o.sales_id from Orders o
-    inner join Company c on c.com_id = o.com_id
-    where c.name = 'RED'
-)
