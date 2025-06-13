d5de1fe44444c13dfef9c9c51430d1c7393095c2
Daniel Marques <dmmarques2005@gmail.com>
2025-06-12 21:37:20 -0300

    Delete 58-length-of-last-word directory

--- 58-length-of-last-word/length-of-last-word.cs
+++
@@ -1,8 +0,0 @@
-public class Solution {
-    public int LengthOfLastWord(string s) {
-    string[] words = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-    
-    return words.Length > 0 ? words[^1].Length : 0;
-}
-
-}
