32e2687d242f91bdc822339e00e4799301aa06c3
Daniel Marques <dmmarques2005@gmail.com>
2025-06-12 21:15:42 -0300

    Delete 1161-project-employees-i directory

--- 1161-project-employees-i/project-employees-i.sql
+++
@@ -1,6 +0,0 @@
-/* Write your T-SQL query statement below */
-select p.project_id, ROUND(avg(e.experience_years * 1.0), 2) as average_years 
-from Project p
-left join Employee e on e.employee_id = p.employee_id
-group by p.project_id
-order by p.project_id
