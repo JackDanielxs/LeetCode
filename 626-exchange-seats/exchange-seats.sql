9f9aebaabd74f027f7c75bbff0d41d4438125188
Daniel Marques <dmmarques2005@gmail.com>
2025-06-12 21:40:42 -0300

    Delete 626-exchange-seats directory

--- 626-exchange-seats/exchange-seats.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-SELECT id,
-CASE WHEN id%2 = 0 THEN
-LAG(student,1) OVER (ORDER BY id)
-ELSE
-IFNULL(LEAD(student,1) OVER (ORDER BY id),student)
-END AS student
-FROM Seat
