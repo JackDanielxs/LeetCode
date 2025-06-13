77ac0793807d05234817f0e8da231643b4d9e740
Daniel Marques <dmmarques2005@gmail.com>
2025-06-12 21:18:10 -0300

    Delete 1268-market-analysis-i directory

--- 1268-market-analysis-i/market-analysis-i.sql
+++
@@ -1,6 +0,0 @@
-/* Write your T-SQL query statement below */
-select u.user_id as buyer_id, u.join_date, 
-(select count(*) from Orders where order_date between '2019-01-01' and '2019-12-31' and buyer_id = u.user_id) as orders_in_2019 
-from Users u
-left join Orders o on o.buyer_id = u.user_id
-group by u.user_id, u.join_date
