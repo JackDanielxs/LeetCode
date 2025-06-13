43e712d4ab3e6106d4ecf83050be9d8f75051fb0
Daniel Marques <dmmarques2005@gmail.com>
2025-06-12 21:37:35 -0300

    Delete 585-investments-in-2016 directory

--- 585-investments-in-2016/investments-in-2016.sql
+++
@@ -1,4 +0,0 @@
-/* Write your T-SQL query statement below */
-select ROUND(SUM(i.tiv_2016),2) as tiv_2016 from Insurance i
-where i.tiv_2015 in (select tiv_2015 from Insurance where pid <> i.pid)
-and CONCAT(i.lat, ' ', i.lon) not in (select CONCAT(lat, ' ', lon) from Insurance where pid <> i.pid)
