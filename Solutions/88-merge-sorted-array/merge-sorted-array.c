196f1398092b46f6c3a184113e61966aab0aca5c
Daniel Marques <dmmarques2005@gmail.com>
2025-06-18 18:52:10 -0300

    Time: 0 ms (100.00%) | Memory: 10.1 MB (25.68%) - LeetSync

---
+++ Solutions/88-merge-sorted-array/merge-sorted-array.c
@@ -0,0 +1,16 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    for (int i = 0; i < n; i++) {
+        nums1[m + i] = nums2[i]; // pusha nums2 no final de nums1
+    }
+    // bubblesort
+    int total = m + n;
+    for (int i = 0; i < total - 1; i++) {
+        for (int j = 0; j < total - i - 1; j++) {
+            if (nums1[j] > nums1[j + 1]) {
+                int temp = nums1[j];
+                nums1[j] = nums1[j + 1];
+                nums1[j + 1] = temp;
+            }
+        }
+    }
+}
